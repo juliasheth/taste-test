4bcab372a58343b30f157cff5316e21bc55c6ee8
Julia Sheth <juliasheth@Julias-MacBook-Air-2.local>
2026-03-21 22:45:04 -0400

    added web analytics to Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { Analytics } from "@vercel/analytics/react";
 
 const SUPABASE_URL = import.meta.env?.VITE_SUPABASE_URL || "";
 const SUPABASE_ANON_KEY = import.meta.env?.VITE_SUPABASE_ANON_KEY || "";
